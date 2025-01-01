c7b4b8fb7b3814913c55914e1bcaf9daaad81b5c
WaqasMangi123 <waqasmangi123@gmail.com>
2025-01-01 23:44:45 +0500

    updating cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS options configuration
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://polar-painting-services-frontend.onrender.com/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
