69c4e9d3d93ecc767c760eadbb1f40cf71e80d7a
WaqasMangi123 <waqasmangi123@gmail.com>
2025-01-01 23:58:55 +0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS options configuration
 const corsOptions = {
-  origin: 'https://polar-painting-services-frontend.onrender.com/',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
